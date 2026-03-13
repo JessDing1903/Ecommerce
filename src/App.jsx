2f37e9bc78bc206cb8785cf7974fa88a4cb47646
JessDing1903 <jessdingamtar@gmail.com>
2026-03-13 17:37:57 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage'; 
+import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+    <Route path="/" element={<HomePage/>}></Route>
+    <Route path="checkout" element={<div>Test checkout page</div>}></Route>
+    </Routes>
+    
+  )
+}
+
+export default App
